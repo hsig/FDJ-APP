2eab4c8e9fc0afe1374bb7cea7ef181fdd526fff
Hamza SG <hsig@users.noreply.github.com>
2016-12-02 09:24:07 +0100

    update MAinApp

--- public/App/MainApp.js
+++ public/App/MainApp.js
@@ -6,7 +6,7 @@ var app = angular.module('FDJ', ['ngRoute','appServices','appControllers']);
 app.config(['$routeProvider', function($routeProvider) {
     $routeProvider.
       when('/balance', {
-        templateUrl: './app/views/balance/balance.html',
+        templateUrl: './App/views/balance/balance.html',
         controller: 'balanceCtrl'
       })
       .otherwise({
